4c6564c4b1ce792ee78a20114e92ce58df47981e
BLVCK <63218178+adithziva@users.noreply.github.com>
2021-06-30 12:30:04 +0530

    Add files via upload

---
+++ blvck.sh
@@ -0,0 +1,133 @@
+cowsay -f dragon Mr_ZIVA | lolcat
+figlet -f standard BLACK | lolcat
+echo -e "\e[96m"
+echo " "
+echo "                      "⚡SINCE:2004⚡
+echo " "
+echo "";
+echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+echo -e ' [#] WE CAN BREAK SECURITY NOT HEART [#] \033[0m';
+echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+
+echo -e '\e[0;35m         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+echo -e '\034                 [#]  INSTAGRAM : MR_ZIVA_ ';
+echo -e '\e[0;35m         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+figlet -f slant "Started downloading"| lolcat
+
+
+git Clone 
+https://github.com/TechnicalMujeeb/Termux-Lazyscript
+
+ls
+
+bash setup.sh
+
+apt update
+
+apt upgrade -y
+
+pkg update -y
+
+pkg upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+pkg install python2-dev -y
+
+pkg install python3 -y
+pkg install java -y
+
+pkg install fish -y
+
+pkg install ruby -y
+
+pkg install help -y
+
+pkg install git -y
+
+pkg install host -y
+
+pkg install php -y
+
+pkg install perl -y
+
+pkg install nmap -y
+
+pkg install bash -y
+
+pkg install clang -y
+
+pkg install nano -y
+
+pkg install w3m -y
+
+pkg install havij -y
+
+pkg install hydra -y
+
+pkg install figlet -y
+
+pkg install cowsay -y
+
+pkg install curl -y
+
+pkg install tar -y
+
+pkg install zip -y
+
+pkg install unzip -y
+
+pkg install tor -y
+
+pkg install google -y
+
+pkg install sudo -y
+
+kg install wget -y
+
+pkg install wireshark -y
+
+pkg install wgetrc -y 
+
+pkg install wcalc -y
+
+pkg install bmon -y
+
+pkg install vpn -y
+
+pkg install unrar -y
+
+pkg install toilet -y
+
+pkg install proot -y
+
+pkg install net-tools -y
+
+pkg install golang -y
+
+pkg install chroot -y
+
+termux-chroot -y
+
+pkg install macchanger -y
+
+pkg install openssl -y
+
+pkg install cmatrix -y
+
+pkg install openssh -y
+
+pkg install wireshark -y
+
+termux-setup-storage -y
+
+pkg install macchanger -y
+apt update && apt upgrade -y
+
+pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install hydra -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade -y 
+
+
+
+
+figlet -f standard download completed... | lolcat
