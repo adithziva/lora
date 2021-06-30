b9930c3b62ab6692fe653fabd1c078efd3810f67
adithziva <adithziva12@gmail.com>
2021-06-30 12:37:51 +0530

    lora

---
+++ lor/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title></title>
+</head>
+
+<body>
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Download LOra Apk">
+  <meta property="og:image" content="bg.html">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="628">
+  <meta property="og:title" content="Download Lora app">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="Download LOra Apk lastest">
+  <meta property="og:url" content="index.html">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#33ffd6">
+
+  <title>Download LOra apk</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../external.html?link=https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+                                      
+                            
+    <!-- Sidebar -->
+    
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+
+        </nav>
+        <!-- End of Topbar -->
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+
+
+          <!-- Content Row -->
+          
+
+          <!-- Content Row -->
+
+          
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Content Column -->
+            
+            <div class="col-lg-12 mb-4">
+
+              <!-- Illustrations -->
+              <div class="card shadow mb-4">
+<hr>
+                
+                <div class="card-body">
+                  <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-1 mb-4" src="https://img.icons8.com/material-outlined/192/000000/whatsapp--v1.png" alt="">
+                    <p>LOra What'sApp</p>
+                    <p>VERSION : 1 </p>
+                     <p>Last Update : 28.06.21 </p>
+                    <p class="bottom">45. MB .Apk</p>
+<a href="./download.php?file=Lorav1.apk" class="btn btn-lg btn-success"> DOWNLOAD APK</a><hr>
+
+<a href="download.php?file=Lorav1.apk">exampleA.zip</a>
+<p align="center">
+
+<p>
+If you need any help contact developer <a href="http://instagram.com/mr_ziva_">Contact me</a>?</p>
+
+</p>
+</body>
+</html>
+
+
+              </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>© Copyright Dezyn by Ziva</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+
+</body>
+</body>
+</html>
